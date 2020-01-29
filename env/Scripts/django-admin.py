75efc1e85bc01e6a4304634cda58538b5913ec7b
Jana Bin Ail <jwalbinali@gmail.com>
2020-01-29 11:58:28 +0300

    tasksevendone

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jana bin ail\desktop\development\taskseven\task_07\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
